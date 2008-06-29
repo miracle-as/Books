74f0840c05c13d700347650c53c6ffc9e348580e
Lars Sehested <larssg@gmail.com>
2008-06-29 23:42:13 +0200

    Translate x days ago

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -2,4 +2,13 @@ module BooksHelper
   def author_names(authors)
     authors.sort_by(&:name).collect { |a| link_to a.name, a }.to_sentence(:skip_last_comma => true, :connector => 'and'[:sentence_connector])
   end
+  
+  def days_ago(date)
+    days = (Date.today - date.to_date)
+    if days == 1
+      'for 1 day ago'[:for_1_day_ago]
+    else
+      'for {days} days ago'[:for_x_days_ago, days]
+    end
+  end
 end
