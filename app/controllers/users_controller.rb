4827b01cd4af6330c1c8f441aa962bee66f9da8b
Lars Sehested <larssg@gmail.com>
2008-06-30 00:52:21 +0200

    Added missing translations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      flash[:notice] = 'Changes to user saved.'[]
+      flash[:notice] = 'Changes to user saved.'[:user_saved]
       redirect_to edit_user_url(@user)
     else
       render :action => 'edit'
