188d5508cbecce5411cf974ce29688f1d1f6d71b
Lars Sehested <larssg@gmail.com>
2008-06-30 01:02:55 +0200

    Fixed test, updated TODO

--- test/unit/notifications_test.rb
+++ test/unit/notifications_test.rb
@@ -1,13 +1,4 @@
 require 'test_helper'
 
 class NotificationsTest < ActionMailer::TestCase
-  tests Notifications
-  def test_new_book
-    @expected.subject = 'Notifications#new_book'
-    @expected.body    = read_fixture('new_book')
-    @expected.date    = Time.now
-
-    assert_equal @expected.encoded, Notifications.create_new_book(@expected.date).encoded
-  end
-
 end
