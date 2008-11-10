b24843e941890d6dcd993dd9dd8cf90dd6be2332
Lars G. Sehested <lgs@lenio.dk>
2008-11-10 10:08:00 +0100

    Move sessions to the database

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081109141750) do
+ActiveRecord::Schema.define(:version => 20081110090739) do
 
   create_table "authors", :force => true do |t|
     t.string   "name"
@@ -76,6 +76,16 @@ ActiveRecord::Schema.define(:version => 20081109141750) do
   end
 
   add_index "searches", ["keywords"], :name => "index_searches_on_keywords"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id",        :limit => 11
