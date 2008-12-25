6c818de7a7bd739005a02db1aa573a32922ccd6d
Lars G. Sehested <lgs@lenio.dk>
2008-12-25 23:18:00 +0100

    prototype -> jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,3 @@
-function nearBottomOfPage() {
-  return scrollDistanceFromBottom() < 80;
-}
-
-function scrollDistanceFromBottom(argument) {
-  return pageHeight() - (window.pageYOffset + self.innerHeight);
-}
-
-function pageHeight() {
-  return Math.max(document.body.scrollHeight, document.body.offsetHeight);
-}
-
 $(document).ready(function() {
   $('.cover a').lightBox();
 });
